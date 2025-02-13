d15f81ea85386d943afdaac1e59d34e23b947200
erenserpin <erenserpin@gmail.com>
2025-02-13 16:33:47 -0500

    updated logo, changed nav bar for mobile

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../images/Logo.png";
+import logo from "../images/Logo.jpeg";
 import {Link} from 'react-router-dom';
 import { HashLink} from "react-router-hash-link";
 const NavBar = () => {
