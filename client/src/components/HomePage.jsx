cff5d166db51d076fe171430cd4faaa5406edf70
erenserpin <erenserpin@gmail.com>
2025-02-18 13:19:45 -0500

    styling changes for home and buy-a-copy

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -5,7 +5,7 @@ import promo from "../images/Promo.jpg";
 import {Link} from 'react-router-dom';
 export const HomePage = () => {
   return (
-    <div id="body">
+    <div id="body" className="home-page">
       <NavBar />
       <section id="home">
         <img id="homeImg"
@@ -23,13 +23,23 @@ export const HomePage = () => {
       <h2>Nandi and the Castle in the Sea</h2>
         <p>
         "Nature often seems subject to the whims of mankind, but in looking at the big picture, one may find the opposite to be true."
-In the past, whenever there was a threat to the people of Crescent Island, they would receive a blessing in the form of two Gems-one Red, and one Blue.
-The Red Gem would grant it's holder incredible powers, and near-immortality. However, the Gem would unleash a great, dark temptation within the bearer, to keep the power for themself. Thus, the bearer of the Blue Gem had but one responsibility; to hold their counterpart responsible. If both Gems were not returned to their rightful place, there would eventually be grave consequences for all.
-In the present day however, the island is ruled by Verden, who bears the Red Gem, and has done so for the last 30 years. No one seems to know what exactly happened to the Blue Gem, or where it even is.
-Verden has opened Crescent Island's borders to the world, welcoming travelers from all over to experience the island's wonders, which include exotic wildlife. homes in the trees, and a mysterious black castle that juts out of the sea, just off the shore.
-Our story follows a young local boy named Nandi, who spends his days with his crew, catching waves and hustling tourists on the beach.
-Nandi is generally content with this tranquil lifestyle. Whenever he's in the water, though, he gets a strange feeling, as if the castle is calling
-out to him...
+        </p>
+        <p>
+          In the past, whenever there was a threat to the people of Crescent Island, they would receive a blessing in the form of two Gems-one Red, and one Blue.
+        </p>
+        <p>
+          The Red Gem would grant it's holder incredible powers, and near-immortality. However, the Gem would unleash a great, dark temptation within the bearer, to keep the power for themself. Thus, the bearer of the Blue Gem had but one responsibility; to hold their counterpart responsible. If both Gems were not returned to their rightful place, there would eventually be grave consequences for all.
+        </p>
+        <p>
+          In the present day however, the island is ruled by Verden, who bears the Red Gem, and has done so for the last 30 years. No one seems to know what exactly happened to the Blue Gem, or where it even is.
+        </p>
+        <p>
+          Verden has opened Crescent Island's borders to the world, welcoming travelers from all over to experience the island's wonders, which include exotic wildlife. homes in the trees, and a mysterious black castle that juts out of the sea, just off the shore.
+        </p>
+        <p>
+          Our story follows a young local boy named Nandi, who spends his days with his crew, catching waves and hustling tourists on the beach.
+          Nandi is generally content with this tranquil lifestyle. Whenever he's in the water, though, he gets a strange feeling, as if the castle is calling
+          out to him...
         </p>
         <Link to="/buy-a-copy">
             <button className="btn-read">Buy Now</button>
@@ -38,7 +48,7 @@ out to him...
       <section id="sellers" class="home-page-section">
         <h2>Want to sell our book?</h2>
         <p>
-          Contact us at slow.comics.publishing@gmail.com
+          Email us at slow.comics.publishing@gmail.com
         </p>
       </section>
       <Footer />
