27c4e150fed58e97747e11b1adb0ebca70e13567
Eren Serpin <erenserpin@gmail.com>
2025-04-09 13:58:41 -0400

    Styled navbar to have hamburger menu when on mobile

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,31 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../images/logo2.png";
 import {Link} from 'react-router-dom';
 import { HashLink} from "react-router-hash-link";
+import { FaBars, FaTimes } from "react-icons/fa";
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
+  const toggleNav = () => setIsOpen(!isOpen);
+  const closeNav = () => setIsOpen(false);
   return (
-    <nav>
-      <Link to="/" >
-      <img id="nav-logo" src={logo} alt="Logo" />
+    <nav className="navbar">
+      <Link to="/" onClick={closeNav}>
+        <img id="nav-logo" src={logo} alt="Logo" />
       </Link>
-      <ul className="navLinks"> {/* Add the class here */}
-        <li><Link to="/">Home</Link></li>
-        <li><HashLink
+
+      <div className="hamburger" onClick={toggleNav}>
+        {isOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+      <ul className={`navLinks ${isOpen ? "open" : ""}`}>
+        <li><Link to="/" onClick={closeNav}>Home</Link></li>
+        <li>
+          <HashLink
             to="/#about"
             smooth
             scroll={(el) => el.scrollIntoView({ behavior: "smooth", block: "start" })}
+            onClick={closeNav}
           >
             About Us
-          </HashLink></li>
-        <li><Link to="/buy-a-copy">Shop</Link></li>
-        <li><a href="#contact">Contact Us</a></li>
-        <li><Link to="/read" >Read Online</Link></li>
+          </HashLink>
+        </li>
+        <li><Link to="/buy-a-copy" onClick={closeNav}>Shop</Link></li>
+        <li><a href="#contact" onClick={closeNav}>Contact Us</a></li>
+        <li><Link to="/read" onClick={closeNav}>Read Online</Link></li>
       </ul>
     </nav>
   );
 };
 
-
-
 export default NavBar;
+
