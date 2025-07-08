91538ca1c770acb3f5228e2a7412afa86b208c79
Eren Serpin <erenserpin@gmail.com>
2025-07-08 16:46:53 -0400

    Last change to post-purchase message to clarify

--- client/src/components/ThankYou.jsx
+++ client/src/components/ThankYou.jsx
@@ -5,6 +5,7 @@ export const ThankYou = () => (
   <div className="thank-you-container">
     <h1>Thank You for Your Order!</h1>
     <p>A confirmation email has been sent to the email you provided.</p>
+    <p>If you purchased a digital copy, you received an email that contains a download link. The link is valid for 48 hours and can be used up to 3 times.</p>
     <Link to="/">Return to Home</Link>
   </div>
 );
